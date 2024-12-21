7c2d6604bb2943c17feb7e18a1e26b15ffd815a2
Heli Patel <helip0269@gmail.com>
2024-12-21 16:48:14 -0500

    Initial commit

---
+++ src/assets/data.js
@@ -0,0 +1,47 @@
+export const data = [
+    {
+        question: "What does HTML stand for?",
+        option1: "HyperText Markup Language",
+        option2: "HighTech Machine Language",
+        option3: "Home Tool Markup Language",
+        option4:  "HyperLink Machine Language",
+        ans: 1,
+
+    },
+    {
+        question: "Which programming language is primarily used for web development on the client-side?",
+        option1: "Python",
+        option2: "JavaScript",
+        option3: "C++",
+        option4: "Java",
+        ans: 2,
+
+    },
+    {
+        question: "What is the primary function of an operating system?",
+        option1: "To manage hardware and software resources",
+        option2: "To compile programs into machine code",
+        option3: "To edit graphics and videos",
+        option4: "To browse the internet",
+        ans: 1,
+
+    },
+    {
+        question: "What does 'IP' in IP address stand for?",
+        option1: "Internet Protocol",
+        option2: "Internal Process ",
+        option3: "Internet Provider",
+        option4: "Information Path",
+        ans: 1,
+
+    },
+    {
+        question: "What is the basic unit of data in computing?",
+        option1: "Byte ",
+        option2: "Bit" ,
+        option3: "Kilobyte ",
+        option4: "Megabyte",
+        ans: 2,
+
+    },
+];
