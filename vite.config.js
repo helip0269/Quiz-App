117a496df3a77c3f3a8cfa7cbf1b85ec416b67ae
Heli Patel <helip0269@gmail.com>
2025-07-02 18:04:39 -0400

    Added Docker support and updated Vite config

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Quiz-App/', // Replace <repository-name> with your repo name
+  base: './', 
 });
